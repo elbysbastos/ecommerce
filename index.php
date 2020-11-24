f75798e2d297ab438eca2be5852625bab781a27d
Elbys Bastos <elbysbastos@gmail.com>
2020-11-23 23:51:31 -0300

    criamos a classe page

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 
 require_once("vendor/autoload.php");
 
+use \Slin\Slin;
+use  \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
+
+	$page = new Page();
+
+	$page->setTpl("index");
 	
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-
-
 });
 
 $app->run();
