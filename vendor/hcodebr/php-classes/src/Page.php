ad535c3224950cd086d9c2040e41d6ed73067537
Elbys Bastos <elbysbastos@gmail.com>
2020-11-26 13:06:35 -0300

    programando login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ namespace Hcode;
 		private $tpl;
 		private $options=[];
 		private $defaults = [
+			"header"=>true,
+			"footer"=>true,
 			"data"=>[]
 		];
 
@@ -27,7 +29,7 @@ namespace Hcode;
 
 			$this->setData($this->options["data"]);
 
-			$this->tpl->draw("header");
+			if ($this->options["header"] === true) $this->tpl->draw("header");
 
 		}
 		
@@ -49,7 +51,7 @@ namespace Hcode;
 
 		public function __destruct(){
 
-			$this->tpl->draw("footer");
+			if ($this->options["footer"] === true)$this->tpl->draw("footer");
 
 		}
 	}
